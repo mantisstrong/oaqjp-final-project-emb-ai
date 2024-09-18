223bd76aa1b72cea3668dda717550a027254f06f
mantisstrong <mfund4929@gmail.com>
2024-09-18 16:47:08 -0500

    Add files via upload

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,4 +1,4 @@
-let RunSentimentAnalysis = ()=>{
+/*let RunSentimentAnalysis = ()=>{
     textToAnalyze = document.getElementById("textToAnalyze").value;
 
     let xhttp = new XMLHttpRequest();
@@ -9,4 +9,30 @@ let RunSentimentAnalysis = ()=>{
     };
     xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
     xhttp.send();
+}
+*/
+
+let RunSentimentAnalysis = () => {
+    const textToAnalyze = document.getElementById("textToAnalyze").value;
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                const response = JSON.parse(xhttp.responseText);
+                document.getElementById("system_response").innerHTML = `
+                    <p>Dominant Emotion: ${response.dominant_emotion}</p>
+                    <pre>${JSON.stringify(response.emotions, null, 2)}</pre>
+                `;
+            } else {
+                const errorResponse = JSON.parse(xhttp.responseText);
+                document.getElementById("system_response").innerHTML = `
+                    <p>Error: ${errorResponse.error}</p>
+                `;
+            }
+        }
+    };
+    xhttp.open("POST", "/analyze", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify({ text: textToAnalyze }));
 }
